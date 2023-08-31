b95841b7ca9a31dae565de02bfa7a244b0e5f92e
ArthurOReis <121455711+ArthurOReis@users.noreply.github.com>
2023-08-31 07:53:27 -0300

    Adicionando projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.png';
 
-function App() {
+export default function App() {
+  const introducao = "Gourmet Express é um cardápio online para clientes em estabelecimentos.";
+
+  function logoGourmetExpress() {
+    return logo
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Gourmet Express</h1>
+      <img src={logoGourmetExpress()} alt="Logo do Gourmet express"/>
+      <p>{introducao}</p>
+      <p>Seu objetivo é facilitar ações para clientes e funcionários, automatizando ações como</p>
+      <h2>Para <strong>Clientes</strong>:</h2>
+      <ul>
+        <li>Realizar pedidos;</li>
+        <li>Fazer pagamentos;</li>
+        <li>Acessar cardápio com antecedência;</li>
+      </ul>
+      <h2>Para <strong>Funcionários</strong>:</h2>
+      <ul>
+        <li>Receber pedidos e entregá-los;</li>
+        <li>Fazer confirmação de entrega de pedidos via código (QR COde ou token);</li>
+        <li>Ter acesso a gráficos que mostrem a quantia de vendas dos pratos;</li>
+      </ul>
     </div>
   );
 }
-
-export default App;
